518a33bcc27262866484f3a07703f0a07165521b
Steffen Winternheimer <s.winternheimer@fb-pro.com>
2023-09-11 15:27:44 +0200

    Update Sleep-Time for DSC State (20 sec -> 5 sec)

--- SystemValidator.ps1
+++ SystemValidator.ps1
@@ -612,7 +612,7 @@ function Create-HTMLBody {
             $lcmConfigs = Get-DscLocalConfigurationManager
             if($lcmConfigs.RefreshMode -ne "Disabled"){
                 while($lcmConfigs.LCMStateDetail -ne ""){
-                    Start-Sleep -Seconds 20
+                    Start-Sleep -Seconds 5
                     Write-Host "LCM is in status '$($lcmConfigs.LCMStateDetail)', waiting..."
                     $lcmConfigs = Get-DscLocalConfigurationManager
                 }
