9f87c68a197d498c245b667437569fe899f2d832
Johannes Laux <Johannes.laux@fb-pro.com>
2025-08-21 13:17:00 +0200

    Update SystemValidator.ps1

--- SystemValidator.ps1
+++ SystemValidator.ps1
@@ -640,7 +640,7 @@ function Create-HTMLBody {
                 ConfigurationCheck "WSManStackVersion" $psVTable.WSManStackVersion "info" ""
                 ConfigurationCheck "PSRemotingProtocolVersion" $psVTable.PSRemotingProtocolVersion "info" ""
                 ConfigurationCheck "SerializationVersion" $psVTable.SerializationVersion "info" ""
-                ConfigurationCheck "(Effective ExecutionPolicy) ExecutionPolicy" "$(Get-ExecutionPolicy)" "in" @("RemoteSigned", "Undefined", "Unrestricted", "Bypass")
+                ConfigurationCheck "(Effective ExecutionPolicy) ExecutionPolicy" $(Get-ExecutionPolicy) "in" @("RemoteSigned", "Undefined", "Unrestricted", "Bypass")
                 ConfigurationCheck "(ExecutionPolicy) MachinePolicy" $($policies[0].ExecutionPolicy) "info" ""
                 ConfigurationCheck "(ExecutionPolicy) UserPolicy" $($policies[1].ExecutionPolicy) "info" ""
                 ConfigurationCheck "(ExecutionPolicy) Process" $($policies[2].ExecutionPolicy) "info" ""
